f72d709ecc37201e31583ccaaf14d8d13fe65997
shiva <97592150+shiva8504@users.noreply.github.com>
2022-09-10 11:39:27 +0530

    import java.util.Scanner; class rambabu {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.printf("%d",a%b);     }
    }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class rambabu
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.printf("%d",a%b);
+    }
+    
+}
