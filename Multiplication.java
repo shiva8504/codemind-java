7730136080892e19c19351dcedfc728ce0f8cc84
shiva <97592150+shiva8504@users.noreply.github.com>
2022-09-10 10:52:48 +0530

    import java.util.Scanner; class Mul {     public static void main(String[]args){         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.printf("%d",a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Mul
+{
+    public static void main(String[]args){
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.printf("%d",a*b);
+    }
+}
