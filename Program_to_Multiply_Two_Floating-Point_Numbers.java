33a8908e5e0c98f0e4b3cd33ac53dd80d1820949
shiva <97592150+shiva8504@users.noreply.github.com>
2022-09-10 10:59:38 +0530

    import java.util.Scanner; class Jw {     public static void main(String[] args)     {         float a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Jw
+{
+    public static void main(String[] args)
+    {
+        float a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
