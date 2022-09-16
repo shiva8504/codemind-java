02695c8de2072f9646301c220bc5b87afe860919
shiva <97592150+shiva8504@users.noreply.github.com>
2022-09-16 11:33:38 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         int a,b,c;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         System.out.println(a*b*c*512*2);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class java
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println(a*b*c*512*2);
+    }
+}
