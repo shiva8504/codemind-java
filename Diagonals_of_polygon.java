10bbcdc5a72e99fe369d4eb918bf8ccbce08e383
shiva <97592150+shiva8504@users.noreply.github.com>
2022-10-14 10:01:11 +0530

    import java.util.Scanner; class dop {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.print(n*(n-3)/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class dop
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(n*(n-3)/2);
+    }
+}
