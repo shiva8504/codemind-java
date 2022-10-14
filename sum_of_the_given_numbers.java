88f74e5d107661e420ccf34e7ea43316ce0371a8
shiva <97592150+shiva8504@users.noreply.github.com>
2022-10-14 11:41:27 +0530

    import java.util.*; class son{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++){             int v=sc.nextInt();             int k=sc.nextInt();             System.out.println(v+k);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class son{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int v=sc.nextInt();
+            int k=sc.nextInt();
+            System.out.println(v+k);
+        }
+    }
+}
