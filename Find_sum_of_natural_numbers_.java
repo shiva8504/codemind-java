92088dd14adac53d05ca80baa633bab7e7454a17
shiva <97592150+shiva8504@users.noreply.github.com>
2022-10-28 10:05:11 +0530

    import java.util.Scanner; class q {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.println((int)(n*(n+1)/2));     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class q
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println((int)(n*(n+1)/2));
+    }
+}
